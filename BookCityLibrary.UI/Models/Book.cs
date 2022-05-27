9b9650352eef027d8ddecf998c2b7d3b31dfc79f
Vladimir Ivanov <vladimir_magicworld@rambler.ru>
2022-05-28 01:19:44 +0300

    UI: init

---
+++ BookCityLibrary.UI/Models/Book.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookCityLibrary.Models;
+
+public class Book
+{
+    public int Id { get; set; }
+        
+    [Required]
+    public string Title { get; set; }
+        
+    [DisplayName("Year of Publication")]
+    public int Year { get; set; }
+        
+    public string Summary { get; set; }
+        
+    [Required]
+    [MaxLength(14)]
+    public string Isbn { get; set; }
+        
+    public string Image { get; set; } = "https://via/placeholder.com/300x300";
+        
+    public decimal? Price { get; set; }
+
+    public int AuthorId { get; set; }
+        
+    public virtual Author Author { get; set; }
+}
---
+++ BookCityLibrary.UI/Services/BookService.cs
@@ -0,0 +1,15 @@
+﻿using BookCityLibrary.Contracts;
+using BookCityLibrary.Models;
+
+namespace BookCityLibrary.Services;
+
+public class BookService : RepositoryService<Book>, IBookRepository
+{
+    private readonly HttpClient _client;
+
+    public BookService(HttpClient client) 
+        : base(client)
+    {
+        _client = client;
+    }
+}
